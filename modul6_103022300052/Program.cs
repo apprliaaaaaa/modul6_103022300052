7335ff1faf4cc3dd750f9b6c1bc525d1f7839bc6
apprliaaaaaa <liania470@gmail.com>
2025-03-23 20:44:03 +0700

    implementasi design by contract

--- modul6_103022300052/Program.cs
+++ modul6_103022300052/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main()
+    {
+       
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - APRILIANI");
+
+        video.PrintVideoDetails();
+        video.IncreasePlayCount(10);
+
+        video.PrintVideoDetails();
+    }
+}
+
